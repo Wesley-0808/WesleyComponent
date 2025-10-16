a3dbc62d986c7be89e587132b144127eeacbc03b
Wesley <985189328@qq.com>
2025-10-16 14:21:58 +0800

    chore: build

--- scripts/vue-next/index.ts
+++
@@ -1,9 +0,0 @@
-import { buildTypes } from './build-types';
-import { buildComponents } from './build-components';
-
-async function build() {
-  await buildComponents();
-  await buildTypes();
-}
-
-build();
